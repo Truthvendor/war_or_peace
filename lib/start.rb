894274d0080968fbd8bbe3bd057c9515d93d1778
Michael Chickillo <truthvendor@gmail.com>
2020-05-09 13:15:54 -0600

    Added start and turn methods

--- lib/start.rb
+++ lib/start.rb
@@ -8,3 +8,25 @@ class Start
   attr_reader :player1
               :player2
               :
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+
+  end
+
+  def start
+    p "Welcome to War! (or Peace) This game will be played with 52 cards."
+    p "The players today are #{player1.name} and #{player2.name}."
+    p "Type 'GO' to start the game."
+    p "-" * 66
+
+    input = gets.chomp
+    if input.upcase == "GO"
+      turn
+  end
+
+  def turn
+
+  end
+
+end
