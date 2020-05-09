5757e82301182a8149a5648a8ee3710ee3909ede
Michael Chickillo <truthvendor@gmail.com>
2020-05-09 16:12:11 -0600

    Numerous changes to spoils of war test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -177,6 +177,33 @@ class Turntest < Minitest::Test
     assert_equal 3, turn.player2.deck.cards.count
   end
 
+  def test_spoils_of_war_war
+    # skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    turn.pile_cards
+    winner = turn.winner
+    assert_equal [card1, card2, card5, card4, card3, card6], turn.spoils_of_war
+    require "pry"; binding.pry
+    turn.award_spoils(player2)
+    assert_equal 1, turn.player1.deck.cards.count
+    assert_equal 7, turn.player2.deck.cards.count
+  end
+
   def test_award_spoils
     # skip
     card1 = Card.new(:heart, 'Jack', 11)
