40cef1c971b336a7fd2f76b4cc9ec28d6a6b41f4
Michael Chickillo <truthvendor@gmail.com>
2020-05-10 08:29:50 -0600

    Added final test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -45,7 +45,6 @@ class Turntest < Minitest::Test
 
     assert_instance_of Player, turn.player1
     assert_instance_of Player, turn.player2
-
   end
 
   def test_spoils_of_war_starts_empty
@@ -88,6 +87,27 @@ class Turntest < Minitest::Test
     turn = Turn.new(player1, player2)
 
     assert_equal :basic, turn.type
+  end
+
+  def test_type_of_turn_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    assert_equal :war, turn.type
   end
 
   def test_basic_turn_has_a_winner
@@ -250,26 +270,4 @@ class Turntest < Minitest::Test
     assert_equal [card8], turn.player1.deck.cards
     assert_equal [card7], turn.player2.deck.cards
   end
-
-  def test_spoils_of_war_mut_assur_destr
-    # skip
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, '8', 8)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card4, card3, card6, card7])
-
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-
-    turn = Turn.new(player1, player2)
-    turn.pile_cards
-    assert_equal [card8], turn.player1.deck.cards
-    assert_equal [card7], turn.player2.deck.cards
 end
